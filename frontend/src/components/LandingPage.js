a9b35838f9527ffa28ad78c9cec0868afa0297f4
web dev <tejasreeramireddy10@gmail.com>
2026-01-07 11:46:54 +0530

    updating name

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -44,7 +44,7 @@ const LandingPage = () => {
               fontSize: isSmallScreen ? '2rem' : 'clamp(2rem, 5vw, 3rem)',
             }}
           >
-            Welcome to Employee Management System
+            This is Tejasree
           </Typography>
           <Typography
             variant="h6"
